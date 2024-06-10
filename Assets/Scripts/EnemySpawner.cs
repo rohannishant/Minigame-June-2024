8a530de8eb2f68816ef37944e67a9b44ad338e11
AlexanderLubbers <alexanderb.lubbers@gmail.com>
2024-06-09 23:51:02 -0400

    implemented enemy spawning for the tutorial level

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //SerializeField] attribute is used to make the private variables accessible within the Unity editor without making them public 
+    [SerializeField]
+    private GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnEnemy(prefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator spawnEnemy(GameObject enemy)
+    {
+        //instantiate allows for new objects to be spawned in the scene
+        //this function will spawn a enemy with a random location
+        //quaternion.identity means that the new enemy will not have any rotations
+        GameObject newEnemy = Instantiate(enemy, new Vector3(6f, 1f, 0), Quaternion.identity);
+        return null;
+    }
+}
