20d69e47530b79b9fbaca5a5634450eeaa58aada
Rohan Nishant <82231446+rohannishant@users.noreply.github.com>
2024-06-11 13:55:23 -0400

    add player health

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -13,6 +14,13 @@ public class Player : MonoBehaviour
 
     Vector2 move;
 
+    [SerializeField]
+    int health = 10;
+    [SerializeField]
+    Sprite healthFull, healthHalf, healthEmpty;
+    [SerializeField]
+    Image[] healthImages;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +31,42 @@ public class Player : MonoBehaviour
     void Update()
     {
         move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            UpdateHealth(-1);
+        }
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            UpdateHealth(1);
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void UpdateHealth(int amount)
+    {
+        health = Mathf.Clamp(health + amount, 0, 10);
+        UpdateHealthUI();
+    }
+
+    void UpdateHealthUI()
+    {
+        int fullHearts = Mathf.FloorToInt(health / 2f);
+        for (int i = 0; i < fullHearts; i++)
+        {
+            healthImages[i].sprite = healthFull;
+        }
+        if (health % 2 > 0)
+        {
+            healthImages[fullHearts].sprite = healthHalf;
+            fullHearts++;
+        }
+        for (int i = fullHearts; i < 5; i++)
+        {
+            healthImages[i].sprite = healthEmpty;
+        }
     }
 }
