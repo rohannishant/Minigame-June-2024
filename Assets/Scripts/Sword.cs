cf9ffbe3de6cd46088c2ed73f3702578a6fb0ffd
AlexanderLubbers <alexanderb.lubbers@gmail.com>
2024-06-13 01:44:53 -0400

    the skeleton now takes knockback when receiving damage

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,13 +6,22 @@ public class Sword : MonoBehaviour
 {
     [SerializeField]
     int damage;
-
+    [SerializeField]
+    int swordKnockback;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Health health = collision.gameObject.GetComponent<Health>();
         if (health != null && collision.gameObject.GetComponent<Player>() == null)
         {
             health.UpdateHealth(-damage);
+            if(collision.gameObject.name == "skeleton_0(Clone)")
+            {
+                Debug.Log("lets go");
+                Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+                Vector2 location = new Vector2(transform.position.x, transform.position.y);
+                Vector2 knockbackDirection = (rb.position - location).normalized;
+                rb.AddForce(knockbackDirection * swordKnockback, ForceMode2D.Impulse);
+            }
         }
     }
 }
